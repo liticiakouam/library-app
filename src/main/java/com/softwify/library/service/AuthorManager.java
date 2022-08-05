1c0ae544651d1d29fd9ea546c1bc5dad95494999
liticiakouam <108594256+liticiakouam@users.noreply.github.com>
2022-08-05 19:27:10 +0100

    Feature/delete (#1)
    * Ajout de la fonctionnalité pour supprimer
    
    * Nettoyage du code et vérification de tous les cas d'erreurs
    
    * Début du test pour la méthode delete du DAO
    
    * Nettoyage du code et gestion des erreurs

--- src/main/java/com/softwify/library/service/AuthorManager.java
+++ src/main/java/com/softwify/library/service/AuthorManager.java
@@ -2,51 +2,91 @@ package com.softwify.library.service;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.softwify.library.dao.AuthorDao;
 import com.softwify.library.model.Author;
 import com.softwify.library.util.OptionSelector;
 
 public class AuthorManager {
 
+	private static final Logger logger = LogManager.getLogger(AuthorManager.class.getSimpleName());
 	private final AuthorDao authorDAO;
-	OptionSelector optionSelector = new OptionSelector();
+	private final OptionSelector optionSelector;
 
-	public AuthorManager(AuthorDao authorDAO) {
+	public AuthorManager(AuthorDao authorDAO, OptionSelector optionSelector) {
 		this.authorDAO = authorDAO;
+		this.optionSelector = optionSelector;
+	}
+
+	public void manage() {
+		displayAuthors();
+
+		boolean continueSection = true;
+
+		while (continueSection) {
+			System.out.println("\r\n" + "Pour ajouter un nouvel auteur, veuillez saisir : \"add\"\r\n"
+					+ "Pour faire une action sur un auteur, veuillez saisir l'action suivit, d'un espace, puis de l'identifiant de l'auteur.\r\n"
+					+ "Actions possible : Suppression (delete).\r\n" + "\r\n"
+					+ "Ou saisissez \"back\" pour retourner au menu principal \r\n"
+					+ "----------------------------------------------");
+
+			String input = optionSelector.readString();
+			input = input.trim().replaceAll("\\s+", " ");
+			String substring[] = input.split(" ");
+			String option = substring[0];
+
+			switch (option) {
+			case "back": {
+				LibraryMenu.loadApp();
+				continueSection = false;
+				break;
+			}
+			case "delete": {
+				try {
+					int id = Integer.parseInt(substring[1]);
+					boolean deleted = delete(id);
+					if (deleted) {
+						returnToList();
+					}
+				} catch (NumberFormatException e) {
+					logger.error(substring[1]
+							+ "n'est pas un nombre, entrer un nombre représentant l'identifiant de l'auteur !!!");
+				}
+				break;
+			}
+			default:
+				logger.error("L'action que vous avez effectuer n'est pas correcte, veuillez entrer la valeur requise");
+				break;
+			}
+		}
 	}
 
 	public void displayAuthors() {
 		List<Author> authors = authorDAO.getAuthors();
+
 		System.out.println("Liste des auteurs");
 		for (Author author : authors) {
 			System.out.println(author.getId() + " - " + author.getFullName());
 		}
-		System.out.println("\r\n" + "Pour ajouter un nouvel auteur, veuillez saisir : \"add\"\r\n"
-				+ "Pour faire une action sur un auteur, veuillez saisir l'action suivit, d'un espace, puis de l'identifiant de l'auteur.\r\n"
-				+ "Actions possible : Suppression (delete).\r\n" + "\r\n"
-				+ "Ou saisissez \"back\" pour retourner au menu principal \r\n"
-				+ "----------------------------------------------");
-
-		selectNextAction();
 	}
 
-	private void selectNextAction() {
-		boolean continueApp = true;
-		String option = optionSelector.readString();
-		while (continueApp) {
-			switch (option) {
-			case "back": {
-				LibraryMenu.loadApp();
-				break;
-			}
-			case "delete": {
-				break;
-			}
-			default:
-				break;
-			}
+	public boolean delete(int id) {
+		boolean result = authorDAO.deleteAuthor(id);
+
+		if (result) {
+			System.out.println("L'auteur et ses livres ont ete supprimes avec succes.");
+		} else {
+			System.out.println("L'auteur que vous avez choisi, n'existe pas.");
 		}
 
+		return result;
 	}
 
+	private void returnToList() {
+		System.out.println("Tapez \"ENTER\" pour retourner\r\n" + "-------------------------");
+		optionSelector.readString();
+		displayAuthors();
+	}
 }
--- src/test/java/com/softwify/library/AuthorManagerTest.java
+++ src/test/java/com/softwify/library/AuthorManagerTest.java
@@ -1,23 +1,46 @@
 package com.softwify.library;
 
-
-import com.softwify.library.dao.AuthorDao;
-import com.softwify.library.service.AuthorManager;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import com.softwify.library.dao.AuthorDao;
+import com.softwify.library.service.AuthorManager;
+import com.softwify.library.util.OptionSelector;
 
 class AuthorManagerTest {
 
-    AuthorDao authorDAO = Mockito.mock(AuthorDao.class);
-    AuthorManager authorManager = new AuthorManager(authorDAO);
+	AuthorDao authorDao = Mockito.mock(AuthorDao.class);
+	OptionSelector optionSelector = Mockito.mock(OptionSelector.class);
+	AuthorManager authorManager = new AuthorManager(authorDao, optionSelector);
 
-    @Test
-    public void testDisplayAuthors() {
-        authorManager.displayAuthors();
-        verify(authorDAO, times(1)).getAuthors();
-    }
+	@Test
+	public void testDisplayAuthors() {
+		authorManager.displayAuthors();
+		verify(authorDao, times(1)).getAuthors();
+	}
+
+	@Test
+	public void deleteAuthorShouldReturnFalseWhenDaoReturnFalse() {
+		when(authorDao.deleteAuthor(anyInt())).thenReturn(false);
+
+		boolean deleted = authorManager.delete(10);
+		assertFalse(deleted);
+		verify(authorDao, times(1)).deleteAuthor(10);
+	}
+
+	@Test
+	public void deleteAuthorShouldReturnTrueWhenDaoReturnTrue() {
+		when(authorDao.deleteAuthor(anyInt())).thenReturn(true);
+
+		boolean deleted = authorManager.delete(10);
+		assertTrue(deleted);
+		verify(authorDao, times(1)).deleteAuthor(10);
+	}
 }
